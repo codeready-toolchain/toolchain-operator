1b1799fda8fcba5245699a7feee1e4b48eec8c6e
Alexey Kazakov <alkazako@redhat.com>
2019-09-11 17:30:18 -0700

    Add API

---
+++ pkg/apis/toolchain/group.go
@@ -0,0 +1,6 @@
+// Package toolchain contains toolchain API versions.
+//
+// This file ensures Go source parsers acknowledge the toolchain package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package toolchain
---
+++ pkg/apis/toolchain/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the toolchain v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=toolchain.dev.openshift.com
+package v1alpha1
