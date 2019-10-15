3e06492a197017bbd520ae8b8b4017e86f254ff3
Dipak Pawar <dipakpawar231@gmail.com>
2019-10-15 12:27:19 +0530

    fix permission issue

--- pkg/apis/apis.go
+++ pkg/apis/apis.go
@@ -1,7 +1,6 @@
 package apis
 
 import (
-	"github.com/codeready-toolchain/api/pkg/apis"
 	"github.com/codeready-toolchain/toolchain-operator/pkg/apis/toolchain/v1alpha1"
 	"k8s.io/apimachinery/pkg/runtime"
 
@@ -9,11 +8,21 @@ import (
 	olmv1alpha1 "github.com/operator-framework/operator-lifecycle-manager/pkg/api/apis/operators/v1alpha1"
 )
 
+// AddToSchemes may be used to add all resources defined in the project to a Scheme
+var AddToSchemes runtime.SchemeBuilder
+
+func init() {
+	// Register the Toolchain Operator types with the Scheme so the components can map objects to GroupVersionKinds and back
+	AddToSchemes = append(AddToSchemes, v1alpha1.SchemeBuilder.AddToScheme)
+
+	// Register the Subscription types with the Scheme so the components can map objects to GroupVersionKinds and back
+	AddToSchemes = append(AddToSchemes, olmv1alpha1.AddToScheme)
+
+	// Register the OperatorGroup types with the Scheme so the components can map objects to GroupVersionKinds and back
+	AddToSchemes = append(AddToSchemes, olmv1.AddToScheme)
+}
+
 // AddToScheme adds all Resources to the Scheme
 func AddToScheme(s *runtime.Scheme) error {
-	// add olm Subscription Schema from olmv1alpha1
-	// add olm OperatorGroup Schema from olmv1
-	addToSchemes := append(apis.AddToSchemes, olmv1alpha1.AddToScheme, olmv1.AddToScheme, v1alpha1.SchemeBuilder.AddToScheme)
-
-	return addToSchemes.AddToScheme(s)
+	return AddToSchemes.AddToScheme(s)
 }
