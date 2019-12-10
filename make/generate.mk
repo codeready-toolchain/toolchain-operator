abd834c49aadc6570fbe3da8a8c34dcfd8422d01
Xavier Coulon <xcoulon@redhat.com>
2019-12-10 16:25:09 +0100

    regenerated files
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- make/generate.mk
+++ make/generate.mk
@@ -11,27 +11,29 @@ generate: vendor generate-deepcopy generate-openapi generate-crds generate-csv
 generate-deepcopy:
 	@echo "re-generating the deepcopy go file..."
 	$(Q)go run $(shell pwd)/vendor/k8s.io/code-generator/cmd/deepcopy-gen/main.go \
-	--input-dirs ./pkg/apis/$(API_GROUPNAME)/$(API_VERSION)/ -O zz_generated.deepcopy \
-	--bounding-dirs github.com/codeready-toolchain/toolchain-operator/pkg/apis "$(API_GROUPNAME):$(API_VERSION)" \
-	--go-header-file=make/go-header.txt
+		--input-dirs ./pkg/apis/$(API_GROUPNAME)/$(API_VERSION)/ -O zz_generated.deepcopy \
+		--bounding-dirs github.com/codeready-toolchain/toolchain-operator/pkg/apis "$(API_GROUPNAME):$(API_VERSION)" \
+		--go-header-file=make/go-header.txt
 	
 .PHONY: generate-openapi
 generate-openapi:
 	@echo "re-generating the openapi go file..."
 	$(Q)go run $(shell pwd)/vendor/k8s.io/kube-openapi/cmd/openapi-gen/openapi-gen.go \
-	--input-dirs ./pkg/apis/$(API_GROUPNAME)/$(API_VERSION)/ \
-	--output-package github.com/codeready-toolchain/toolchain-operator/pkg/apis/$(API_GROUPNAME)/$(API_VERSION) \
-	--output-file-base zz_generated.openapi \
-	--go-header-file=make/go-header.txt
+		--input-dirs ./pkg/apis/$(API_GROUPNAME)/$(API_VERSION)/ \
+		--output-package github.com/codeready-toolchain/toolchain-operator/pkg/apis/$(API_GROUPNAME)/$(API_VERSION) \
+		--output-file-base zz_generated.openapi \
+		--go-header-file=make/go-header.txt
 
 .PHONY: generate-crds
 generate-crds: vendor 
 	@echo "Re-generating the Toolchain CRD files..."
-	$(Q)go run $(shell pwd)/vendor/sigs.k8s.io/controller-tools/cmd/controller-gen/main.go crd:trivialVersions=true \
-	paths=./pkg/apis/... output:dir=deploy/crds
+	$(Q)go run $(shell pwd)/vendor/sigs.k8s.io/controller-tools/cmd/controller-gen/main.go \
+		crd:trivialVersions=true \
+		paths=./pkg/apis/... \
+		output:dir=deploy/crds
 	# Delete two first lines of the CRD ("\n----\n") to make a single manifest file out of the original multiple manifest file
 	@find deploy/crds -name "toolchain.*.yaml" -exec sed -i '' -e '1,2d' '{}' \; 
-	
+
 PATH_TO_GENERATE_FILE=../api/scripts/olm-catalog-generate.sh
 
 .PHONY: generate-csv
