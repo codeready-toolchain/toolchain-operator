9e2cd5ef72581ad47499a808a2cfcbdd762c1479
Matous Jobanek <mjobanek@redhat.com>
2020-04-02 17:21:02 +0200

    use IMAGE_FORMAT var

--- make/test.mk
+++ make/test.mk
@@ -144,7 +144,7 @@ ifneq ($(IS_OS_3),)
 	$(eval IMAGE_NAME := docker.io/${GO_PACKAGE_ORG_NAME}/${GO_PACKAGE_REPO_NAME}:${GIT_COMMIT_ID_SHORT})
 	$(MAKE) docker-image IMAGE=${IMAGE_NAME}
 else ifneq ($(IS_OS_CI),)
-	$(eval IMAGE_NAME := ${IMAGE_TOOLCHAIN_OPERATOR})
+	$(eval IMAGE_NAME := ${IMAGE_FORMAT}toolchain-operator)
 else
 	# For OpenShift-4
 	$(eval IMAGE_NAME := quay.io/${QUAY_NAMESPACE}/${GO_PACKAGE_REPO_NAME}:${DATE_SUFFIX})
